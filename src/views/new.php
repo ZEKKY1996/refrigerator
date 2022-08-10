32e8dcf49cc2c469caeccded5c4ca7c7c8ac1fb2
root <root@LAPTOP-19L46JU9.localdomain>
2022-08-10 15:49:56 +0900

    bootstrapを導入

--- src/views/new.php
+++ src/views/new.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width,initial-scale=1">
+        <link rel="stylesheet" href="../vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
         <title>冷蔵庫登録画面</title>
         <header>冷蔵庫管理アプリ</header>
     </head>
