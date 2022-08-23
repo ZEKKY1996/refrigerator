23f24e751815ba31a6ba1e4adf01ef8b9686e291
root <root@LAPTOP-19L46JU9.localdomain>
2022-08-23 21:45:35 +0900

    mysqli.phpを修正

--- src/lib/mysqli.php
+++ src/lib/mysqli.php
@@ -7,15 +7,10 @@ function dbConnect(): object
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
     $dotenv->load();
 
-    // $dbHost = $_ENV['DB_HOST'];
-    // $dbUsername = $_ENV['DB_USERNAME'];
-    // $dbPass = $_ENV['DB_PASS'];
-    // $dbDatabase = $_ENV['DB_DATABASE'];
-
-    $dbHost = "db";
-    $dbUsername = "ref";
-    $dbPass = "pass";
-    $dbDatabase = "ref";
+    $dbHost = $_ENV['DB_HOST'];
+    $dbUsername = $_ENV['DB_USERNAME'];
+    $dbPass = $_ENV['DB_PASS'];
+    $dbDatabase = $_ENV['DB_DATABASE'];
 
     $link = mysqli_connect($dbHost, $dbUsername, $dbPass, $dbDatabase);
     if (!$link) {
